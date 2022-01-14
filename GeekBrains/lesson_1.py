f21e47e891b8bfe0eb6cc1077abf559e2b17b1ac
Dmitry <red_r2005@mail.ru>
2022-01-14 19:56:55 +0300

    adding a lesson 8.py

--- GeekBrains/lesson_1.py
+++ GeekBrains/lesson_1.py
@@ -16,29 +16,29 @@ print(f"Число с точкой: {my_float}, Строка: {my_str}, Цело
 # 2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
 # Используйте форматирование строк
 
-# Пользовательский ввод
-# duration = int(input("Введите время в секундах: "))
-#
-# # Определяем константы
-# minute = 60
-# hour = minute * 60
-# day = hour * 24
-#
-# # Для секунд
-# if 0 < duration < 60:
-#     print(f"{duration} сек.")
-#
-# # Для минут
-# elif  minute < duration < hour:
-#     print(f"{duration // minute}:{duration % minute} мин.")
-#
-# # Для часов
-# elif hour < duration < day:
-#     print(f"{duration // hour}:{duration // minute % minute}:{duration % minute} ")
-#
-# # Для дней
-# elif day < duration:
-#     print(f"{duration // day}:{duration % day // hour % minute}:{duration // minute % minute}")
+#Пользовательский ввод
+duration = int(input("Введите время в секундах: "))
+
+# Определяем константы
+minute = 60
+hour = minute * 60
+day = hour * 24
+
+# Для секунд
+if 0 < duration < 60:
+    print(f"{duration} сек.")
+
+# Для минут
+elif  minute < duration < hour:
+    print(f"{duration // minute}:{duration % minute} мин.")
+
+# Для часов
+elif hour < duration < day:
+    print(f"{duration // hour}:{duration // minute % minute}:{duration % minute} ")
+
+# Для дней
+elif day < duration:
+    print(f"{duration // day}:{duration % day // hour % minute}:{duration // minute % minute}")
 
 
 
