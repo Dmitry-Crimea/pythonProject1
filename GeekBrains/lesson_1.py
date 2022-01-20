b4914c81f01603463d016e67ef8437cf2a4f3ff7
it <red_r2005@mail.ru>
2022-01-20 11:38:22 +0300

    start

--- GeekBrains/lesson_1.py
+++ GeekBrains/lesson_1.py
@@ -1,62 +1,82 @@
-# 1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк,
-# и сохраните в переменные, выведите на экран.
+# # 1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и строк,
+# # и сохраните в переменные, выведите на экран.
+#
+# # Создаём переменные и выводим на экран
+# my_int = 5
+# my_str = 'string'
+# my_float = 2.1
+# print(f"Число с точкой: {my_float}, Строка: {my_str}, Целое число: {my_int}")
+#
+# # Просим пользователя ввести значения и выводим их на экран
+# my_int = int(input("Введите целое число: "))
+# my_str = input("Введите строку: ")
+# my_float = float(input("Введите число с точкой: "))
+# print(f"Число с точкой: {my_float}, Строка: {my_str}, Целое число: {my_int}")
+#
+# # 2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
+# # Используйте форматирование строк
+#
+# #Пользовательский ввод
+# duration = int(input("Введите время в секундах: "))
+#
+# # Определяем константы
+# minute = 60
+# hour = minute * 60
+# day = hour * 24
+#
+# # Для секунд
+# if 0 < duration < 60:
+#     print(f"{duration} сек.")
+#
+# # Для минут
+# elif  minute < duration < hour:
+#     print(f"{duration // minute}:{duration % minute} мин.")
+#
+# # Для часов
+# elif hour < duration < day:
+#     print(f"{duration // hour}:{duration // minute % minute}:{duration % minute} ")
+#
+# # Для дней
+# elif day < duration:
+#     print(f"{duration // day}:{duration % day // hour % minute}:{duration // minute % minute}")
+#
+# # 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например,
+# # пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+#
+# n = int(input("Введите цифру: "))     # Запрашиваем ввод у пользователя
+# str_n = str(n)                        # Переводим число в строку
+# str_nn = str_n + str_n                # Конкатенируем строки
+# str_nnn = str_n + str_nn              # --------------------
+# sum_n = int(str_n) + int(str_nn) + int(str_nnn) # Переводим обчратно в "int" и суммируем
 
-# Создаём переменные и выводим на экран
-my_int = 5
-my_str = 'string'
-my_float = 2.1
-print(f"Число с точкой: {my_float}, Строка: {my_str}, Целое число: {my_int}")
-
-# Просим пользователя ввести значения и выводим их на экран
-my_int = int(input("Введите целое число: "))
-my_str = input("Введите строку: ")
-my_float = float(input("Введите число с точкой: "))
-print(f"Число с точкой: {my_float}, Строка: {my_str}, Целое число: {my_int}")
-
-# 2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
-# Используйте форматирование строк
-
-#Пользовательский ввод
-duration = int(input("Введите время в секундах: "))
-
-# Определяем константы
-minute = 60
-hour = minute * 60
-day = hour * 24
-
-# Для секунд
-if 0 < duration < 60:
-    print(f"{duration} сек.")
-
-# Для минут
-elif  minute < duration < hour:
-    print(f"{duration // minute}:{duration % minute} мин.")
-
-# Для часов
-elif hour < duration < day:
-    print(f"{duration // hour}:{duration // minute % minute}:{duration % minute} ")
-
-# Для дней
-elif day < duration:
-    print(f"{duration // day}:{duration % day // hour % minute}:{duration // minute % minute}")
-
-# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn. Например,
-# пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
-n = int(input("Введите цифру: "))     # Запрашиваем ввод у пользователя
-str_n = str(n)                        # Переводим число в строку
-str_nn = str_n + str_n                # Конкатенируем строки
-str_nnn = str_n + str_nn              # --------------------
-sum_n = int(str_n) + int(str_nn) + int(str_nnn) # Переводим обчратно в "int" и суммируем
-
-print(f" Сумма: {str_n} + {str_nn} + {str_nnn} = {sum_n}")
+# print(f" Сумма: {str_n} + {str_nn} + {str_nnn} = {sum_n}")
 
 # 4. Пользователь вводит целое положительное число. Найдите самую большую цифру в числе. Для решения используйте цикл
 # while и арифметические операции.
 
-num = int(input("Введите цыфру: "))
+# num = int(input("Введите цыфру: "))
+num = 3659
+max = 0
+digit = 0
+count = 0
 
-while
+while num > 10:
+    digit = num % 10
+    num //= 10
+    second_digit  = num % 10
+    if d > digit:
+        r = d
+        break
+    elif d < k:
+        r = digit
+        break
+    count += 1
+
+print(r)
+
+
+
+
 
 
 
