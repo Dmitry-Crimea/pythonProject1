444b504a5da0281fe42807f3cab7c6abe30d6a1d
dmitry.brykin <dmitry.brykin@ax-team.com>
2023-02-19 20:46:47 +0300

    Добавил 2-е задание

--- Rebrain/lesson_2.py
+++ Rebrain/lesson_2.py
@@ -13,7 +13,7 @@
 # May 24 19:26:40 PC-00102 rtkit-daemon[1131]: Supervising 5 threads of 2 processes of 1 users.
 
 #1.Скопируйте их к себе и создайте из них список (список строк).
-log_list = [
+log_lst = [
 
     "May 18 11:59:18 PC-00102 plasmashell[1312]: kf.plasma.core: findInCache with a lastModified timestamp of 0 is deprecated",
     "May 18 13:06:54 ideapad kwin_x11[1273]: Qt Quick Layouts: Detected recursive rearrange. Aborting after two iterations.",
@@ -28,7 +28,7 @@ log_list = [
     "May 24 19:26:40 PC-00102 rtkit-daemon[1131]: Supervising 5 threads of 2 processes of 1 users."
 
 ]
-print(log_list[:])
+print(log_lst[:])
 
 #2.1.Создайте алгоритм заполнения словаря, подходящий для любой строчки лога. Словарь должен содержать в себе такую информацию:
 #  - 'time': <дата/время>
@@ -36,5 +36,30 @@ print(log_list[:])
 #  - 'service_name': <имя сервиса>
 #  - 'message': <сообщение лога>
 # Еще раз обращаю ваше внимание на то, что алгоритм заполнения должен быть универсальным для всех данных строк.
-# log_str = "May 18 13:06:54 ideapad kwin_x11[1273]: Qt Quick Layouts: Detected recursive rearrange. Aborting after two iterations."
-# print(log_str)
+log_str = "May 18 13:06:54 ideapad kwin_x11[1273]: Qt Quick Layouts: Detected recursive rearrange. Aborting after two iterations."
+log_lst = log_str.split()
+log_dict = {'time':log_lst[0:3], 'pc_name':log_lst[3:4], 'service_name':log_lst[4:5], 'message':log_lst[9:]}
+print(type(log_lst), type(log_dict))
+
+#2.2. Заполните словарь для одной из строк лога с помощью данного алгоритма, запросив у пользователя номер строки
+# с помощью input(). Выведите на экран информацию из текущего словаря в таком виде:
+#<имя компьютера>: <сообщение>
+log_lst = [
+
+    "May 18 11:59:18 PC-00102 plasmashell[1312]: kf.plasma.core: findInCache with a lastModified timestamp of 0 is deprecated",
+    "May 18 13:06:54 ideapad kwin_x11[1273]: Qt Quick Layouts: Detected recursive rearrange. Aborting after two iterations.",
+    "May 20 09:16:28 PC0078 systemd[1]: Starting PackageKit Daemon...",
+    "May 20 11:01:12 PC-00102 PackageKit: daemon start",
+    "May 20 12:48:18 PC0078 systemd[1]: Starting Message of the Day...",
+    "May 21 14:33:55 PC0078 kernel: [221558.992188] usb 1-4: New USB device found, idVendor=1395, idProduct=0025, bcdDevice= 1.00",
+    "May 22 11:48:30 ideapad mtp-probe: checking bus 1, device 3: \"/sys/devices/pci0000:00/0000:00:08.1/0000:03:00.3/usb1/1-4\"",
+    "May 22 11:50:09 ideapad mtp-probe: bus: 1, device: 3 was not an MTP device",
+    "May 23 08:06:14 PC-00233 kernel: [221559.381614] usbcore: registered new interface driver snd-usb-audio",
+    "May 24 16:19:52 PC-00233 systemd[1116]: Reached target Sound Card.",
+    "May 24 19:26:40 PC-00102 rtkit-daemon[1131]: Supervising 5 threads of 2 processes of 1 users."
+
+]
+user_input = int(input("Введите номер строки: "))
+log_str = log_lst[user_input].split()
+log_dict = {'time':log_str[0:3], 'pc_name':log_str[3:4], 'service_name':log_str[4:5], 'message':log_str[5:]}
+print(' '.join(log_dict['pc_name']) + ' : ' + ' '.join(log_dict['message']))
