55a03bb96da1579f0924a542939a25646420646e
VTB4080031@region.vtb.ru <brykin@dev.vtb.ru>
2023-03-23 13:00:56 +0300

    Добавил задание к 2 уроку

--- Rebrain/lesson_9.py
+++ Rebrain/lesson_9.py
@@ -1,20 +1,18 @@
 import argparse, time, logging, os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--delay', default=1, type=int, help='Задержка в секундах')
-parser.add_argument('--num', default=3, type=int, help='Количество строк лога')
+parser.add_argument('--delay', required=True, type=int, help='Задержка в секундах')
+parser.add_argument('--num', required=True, type=int, help='Количество строк лога')
 args = parser.parse_args()
 
 logging.basicConfig(filename='log_file.log',
                     format='%(asctime)s  %(levelname)s  %(message)s',
                     datefmt='%b %H:%M:%S',
                     level=logging.INFO)
-
-log_dict = {key:value for (key, value) in os.environ.items()}
-print(log_dict.keys())
-
-
-
-for i in range(args.num):
-    logging.info(log_dict.keys())
+count = 0
+for key in os.environ:
+    logging.info(key + '->' + os.environ.get(key))
+    count +=1
+    if count >= args.num:
+        break
     time.sleep(args.delay)
