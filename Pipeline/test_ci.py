1c767b8f64a711adbe952304f664dbafa73d275f
VTB4080031@region.vtb.ru <brykin@dev.vtb.ru>
2023-09-13 21:00:34 +0300

    Добавил задание к 2 уроку

---
+++ Pipeline/test_ci.py
@@ -0,0 +1,15 @@
+# from functions.find_file import find_file
+# # from functions.separate import *
+#
+# result = find_file('buildmatri.txt', './')
+#
+# if result:
+#     with open('supply.json', 'a+') as supply:
+#         supply.write(f'file: {result}')
+#
+#
+# print(type(result))
+
+
+
+
